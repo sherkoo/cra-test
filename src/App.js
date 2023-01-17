49208721d587107f716005d5855e80bc2f615a62
sherkoo <stephenherko@gmail.com>
2023-01-17 06:44:20 -0600

    update deployment

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [realms, setRealms] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://stark-thicket-21421.herokuapp.com/api/realms/all')
+      .then(results => {
+        setRealms(results.data.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
+
+        {realms.map((r, index) => (
+          <div key={index}>{r.name}</div>
+        ))}
+
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
